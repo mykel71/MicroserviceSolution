2b859684f040e983b7d62f7ae83d37d0c89596ca
Michael Shepherd M <shepherd.munemo@outlook.com>
2023-08-11 22:44:04 +0200

    setting up

--- CustomerWebApi/Program.cs
+++ CustomerWebApi/Program.cs
@@ -1,8 +1,21 @@
+using CustomerWebApi;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+/* Database Context Dependency Injection*/
+
+var dbHost = "ZOE";
+var dbName = "dms_customer";
+var dbPassword = "P@ssw0rd121#";
+var connectionString = $"data Source={dbHost}; Initial Catalog={dbName};User ID=sa;Password={dbPassword}";
+builder.Services.AddDbContext<CustomerDbContext>(opt => opt.UseSqlServer(connectionString));
+
+/* ====================================== */
 
 var app = builder.Build();
 
